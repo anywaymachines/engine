4d6819e11dd6a0cba0bc8ad3c5d4f5e5ee6b38e9
Maks_gaming <maksim.klimenko.mail@gmail.com>
2024-10-01 20:22:36 +0300

    materials dataset and utils for later use

---
+++ shared/data/Materials.ts
@@ -0,0 +1,59 @@
+import { Workspace } from "@rbxts/services";
+
+export namespace Materials {
+	const materialTextures: { [key: string]: string | undefined } = {
+		Asphalt: "9930003046",
+		Basalt: "9920482056",
+		Brick: "9920482813",
+		Cardboard: "14108651729",
+		Carpet: "14108662587",
+		CeramicTiles: "17429425079",
+		ClayRoofTiles: "18147681935",
+		Cobblestone: "9919718991",
+		Concrete: "9920484153",
+		CorrodedMetal: "9920589327",
+		CrackedLava: "9920484943",
+		DiamondPlate: "10237720195",
+		Fabric: "9920517696",
+		Foil: "9466552117",
+		Glacier: "9920518732",
+		Glass: "9438868521",
+		Granite: "9920550238",
+		Grass: "9920551868",
+		Ground: "9920554482",
+		Ice: "9920555943",
+		LeafyGrass: "9920557906",
+		Leather: "14108670073",
+		Limestone: "9920561437",
+		Marble: "9439430596",
+		Metal: "9920574687",
+		Mud: "9920578473",
+		Pavement: "9920579943",
+		Pebble: "9920581082",
+		Plaster: "14108671255",
+		Rock: "9920587470",
+		RoofShingles: "119722544879522",
+		Rubber: "14108673018",
+		Salt: "9920590225",
+		Sand: "9920591683",
+		Sandstone: "9920596120",
+		Slate: "9920599782",
+		Snow: "9920620284",
+		Wood: "9920625290",
+		WoodPlanks: "9920626778",
+
+		//** Dead materials */
+		SmoothPlastic: undefined,
+		Neon: undefined,
+		Plastic: undefined,
+		ForceField: undefined,
+	};
+
+	export function getMaterialTexture(material: Enum.Material): string | undefined {
+		return materialTextures[material.Name];
+	}
+
+	export function getMaterialDefaultColor(material: Enum.Material): Color3 {
+		return Workspace.Terrain!.GetMaterialColor(material);
+	}
+}
