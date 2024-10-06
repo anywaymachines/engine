14f40d5b329c20135dee1bcde3b2a257f1a4f8a1
Maks_gaming <maksim.klimenko.mail@gmail.com>
2024-10-06 23:02:47 +0300

    Fake secrets

--- server/Secrets.ts
+++ server/Secrets.ts
@@ -3,12 +3,26 @@ import { HttpService, RunService } from "@rbxts/services";
 export namespace Secrets {
 	const developmentSecrets: Map<string, string> = new Map();
 
+	class FakeSecret {
+		constructor(private value: string) {}
+
+		AddPrefix(prefix: string) {
+			this.value = prefix + this.value;
+			return this.value;
+		}
+
+		AddSuffix(suffix: string) {
+			this.value = this.value + suffix;
+			return this.value;
+		}
+	}
+
 	export function addDevelopmentSecret(name: string, value: string) {
 		developmentSecrets.set(name, value);
 	}
 
 	/** Not printable in non-studio server runs */
-	export function getSecret(name: string): string {
+	export function getSecret(name: string): Secret | FakeSecret {
 		if (RunService.IsStudio()) {
 			const data = developmentSecrets.get(name);
 
@@ -16,9 +30,9 @@ export namespace Secrets {
 
 			$debug(`⚠️ Providing "${name}" development secret`);
 
-			return data;
+			return new FakeSecret(data);
 		}
 
-		return HttpService.GetSecret(name) as unknown as string;
+		return HttpService.GetSecret(name);
 	}
 }
