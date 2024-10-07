dc13349f7549d2a6f54cd7dbd2c46ba5fca47851
i3ym <deniso99@mail.ru>
2024-10-07 12:18:32 +0700

    Revert "Fake secrets"
    This reverts commit 14f40d5b329c20135dee1bcde3b2a257f1a4f8a1.

--- server/Secrets.ts
+++ server/Secrets.ts
@@ -3,26 +3,12 @@ import { HttpService, RunService } from "@rbxts/services";
 export namespace Secrets {
 	const developmentSecrets: Map<string, string> = new Map();
 
-	class FakeSecret {
-		constructor(private value: string) {}
-
-		AddPrefix(prefix: string) {
-			this.value = prefix + this.value;
-			return this.value;
-		}
-
-		AddSuffix(suffix: string) {
-			this.value = this.value + suffix;
-			return this.value;
-		}
-	}
-
 	export function addDevelopmentSecret(name: string, value: string) {
 		developmentSecrets.set(name, value);
 	}
 
 	/** Not printable in non-studio server runs */
-	export function getSecret(name: string): Secret | FakeSecret {
+	export function getSecret(name: string): string {
 		if (RunService.IsStudio()) {
 			const data = developmentSecrets.get(name);
 
@@ -30,9 +16,9 @@ export namespace Secrets {
 
 			$debug(`⚠️ Providing "${name}" development secret`);
 
-			return new FakeSecret(data);
+			return data;
 		}
 
-		return HttpService.GetSecret(name);
+		return HttpService.GetSecret(name) as unknown as string;
 	}
 }
