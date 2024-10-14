537145239a7383d21f7a71a4da6fe36e9fec4d2d
i3ym <deniso99@mail.ru>
2024-10-14 16:44:46 +0700

    Fix PlayerInfo deleting the player

--- shared/PlayerInfo.ts
+++ shared/PlayerInfo.ts
@@ -11,7 +11,7 @@ export class PlayerInfo extends InstanceComponent<Player> {
 	readonly diedEvent = new Signal();
 
 	constructor(player: Player) {
-		super(player);
+		super(player, true, false);
 
 		const playerSpawned = () => {
 			const char = player.Character!;
