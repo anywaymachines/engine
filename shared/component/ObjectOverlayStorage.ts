c4b82d27340692513ed25ad4305de5055ae37991
i3ym <deniso99@mail.ru>
2024-10-22 00:08:58 +0700

    Replace overlay default value with nil on set

--- shared/component/ObjectOverlayStorage.ts
+++ shared/component/ObjectOverlayStorage.ts
@@ -2,6 +2,8 @@ import { Transforms } from "engine/shared/component/Transforms";
 import { ObservableValue } from "engine/shared/event/ObservableValue";
 import type { TransformProps } from "engine/shared/component/Transform";
 
+const defaultIndex = 9999999999;
+
 export class ObjectOverlayStorage<T extends object> {
 	private readonly _value;
 	readonly value;
@@ -21,15 +23,19 @@ export class ObjectOverlayStorage<T extends object> {
 		});
 	}
 
-	constructor(defaultValues: T, changed?: (value: T, prev: T) => void, invokeChangedImmediately = false) {
-		this.order.push(9999999999);
-		this.overlays[9999999999] = [defaultValues, defaultValues];
+	constructor(
+		defaultValues: T,
+		changed?: (value: T, prev: T) => void,
+		private readonly treatDefaultAsUndefined = false,
+	) {
+		this.order.push(defaultIndex);
+		this.overlays[defaultIndex] = [defaultValues, defaultValues];
 
 		this._value = new ObservableValue(defaultValues);
 		this.value = this._value.asReadonly();
 
 		if (changed) {
-			this.value.subscribe(changed, invokeChangedImmediately);
+			this.value.subscribe(changed);
 		}
 	}
 
@@ -60,6 +66,10 @@ export class ObjectOverlayStorage<T extends object> {
 		const metatable: LuaMetatable<Partial<T>> = {
 			__index: (_, key) => rawget(backend, key),
 			__newindex: (_, key, value) => {
+				if (this.treatDefaultAsUndefined && value === this.get(defaultIndex)[key as keyof T]) {
+					value = undefined;
+				}
+
 				rawset(backend, key, value);
 				this._value.set(this.getValues());
 			},
