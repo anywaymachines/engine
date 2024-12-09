c6196bf496af560e53a1a3a8b7db8ee19e31c4a7
i3ym <deniso99@mail.ru>
2024-12-09 19:34:40 +0700

    Make ReadonlyObservableCollection*** ReadonlyObservableValue

--- shared/event/ObservableCollectionTypes.d.ts
+++ shared/event/ObservableCollectionTypes.d.ts
@@ -9,9 +9,13 @@ interface ReadonlyObservableCollection<T extends defined> {
 	size(): number;
 	getArr(): readonly T[];
 }
-interface ReadonlyObservableCollectionArr<T extends defined> extends ReadonlyObservableCollection<T> {
+interface ReadonlyObservableCollectionArr<T extends defined>
+	extends ReadonlyObservableCollection<T>,
+		ReadonlyObservableValue<readonly T[]> {
 	get(): readonly T[];
 }
-interface ReadonlyObservableCollectionSet<T extends defined> extends ReadonlyObservableCollection<T> {
+interface ReadonlyObservableCollectionSet<T extends defined>
+	extends ReadonlyObservableCollection<T>,
+		ReadonlyObservableValue<ReadonlySet<T>> {
 	get(): ReadonlySet<T>;
 }
