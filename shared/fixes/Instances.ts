87c43291cf024d5f4b34040c746504b6f59b6e8b
i3ym <deniso99@mail.ru>
2025-01-09 23:46:11 +0700

    Remove assets from Instances

--- shared/fixes/Instances.ts
+++ shared/fixes/Instances.ts
@@ -1,12 +1,12 @@
-import { ReplicatedStorage } from "@rbxts/services";
-
 export namespace Instances {
-	export const assets = ReplicatedStorage.WaitForChild("Assets") as Folder;
+	export function findChild<T = Instance>(object: Instance, ...path: string[]): T {
+		let ret: Instance = object;
+		for (const part of path) {
+			ret = ret.WaitForChild(part);
+		}
 
-	export function getAssets<T extends object = Folder>() {
-		return assets as T;
+		return ret as T;
 	}
-
 	export function waitForChild<T = Instance>(object: Instance, ...path: string[]): T {
 		let ret: Instance = object;
 		for (const part of path) {
