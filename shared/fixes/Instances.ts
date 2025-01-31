9bdde95de9d06d0311326ef0e2df7c462e6af624
i3ym <deniso99@mail.ru>
2025-01-31 14:41:10 +0700

    Fix Instances.findChild being waitForChild

--- shared/fixes/Instances.ts
+++ shared/fixes/Instances.ts
@@ -1,8 +1,9 @@
 export namespace Instances {
-	export function findChild<T = Instance>(object: Instance, ...path: string[]): T {
-		let ret: Instance = object;
+	export function findChild<T = Instance>(object: Instance, ...path: string[]): T | undefined {
+		let ret: Instance | undefined = object;
 		for (const part of path) {
-			ret = ret.WaitForChild(part);
+			if (!ret) return undefined;
+			ret = ret.FindFirstChild(part);
 		}
 
 		return ret as T;
