87e6d727d3a1b38dfb908c6cf29b7c0ea46378eb
i3ym <deniso99@mail.ru>
2025-02-19 04:33:05 +0700

    Add Action:subscribeAction, :subscribeActionObservable

--- client/Action.ts
+++ client/Action.ts
@@ -1,6 +1,6 @@
 import { Component } from "engine/shared/component/Component";
 import { OverlayValueStorage } from "engine/shared/component/OverlayValueStorage";
-import { ArgsSignal } from "engine/shared/event/Signal";
+import { ArgsSignal, Signal } from "engine/shared/event/Signal";
 import type { ReadonlyObservableValue } from "engine/shared/event/ObservableValue";
 
 /** Represents an action that can be executed by a player using a GuiButton or a key. */
@@ -27,6 +27,37 @@ export class Action<TArgs extends unknown[] = []> extends Component {
 	/** Subscribes a function to the action. */
 	subscribe(func: (...args: TArgs) => void): SignalConnection {
 		return this.action.Connect(func);
+	}
+
+	/** Subscribes this action to execute another, copying its requirements. */
+	subscribeAction(action: Action<TArgs>): SignalConnection {
+		const key = tostring(action);
+
+		const sub1 = this.action.Connect((...args) => action.execute(...args));
+		this.canExecute.and(key, action.canExecute);
+
+		return Signal.connection(() => {
+			sub1.Disconnect();
+			this.canExecute.and(key, undefined);
+		});
+	}
+
+	/** Subscribes this action to execute another, copying its requirements. */
+	subscribeActionObservable(action: ReadonlyObservableValue<Action<TArgs> | undefined>): this {
+		let sub: SignalConnection | undefined = undefined;
+		this.onDestroy(() => sub?.Disconnect());
+		this.event.subscribeObservable(
+			action,
+			(action) => {
+				sub?.Disconnect();
+				if (!action) return;
+
+				sub = this.subscribeAction(action);
+			},
+			true,
+		);
+
+		return this;
 	}
 
 	/** Adds checks to the action's can execute state. Returns this; */
